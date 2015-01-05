8243a574211dbcef9ed1a32a47ac0512d70044dd
Joe Nelson <cred+github@begriffs.com>
2015-01-04 23:52:39 -0800

    WIP: terraform for vpc

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-west-1"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "web" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-1a"
+}
+
+resource "aws_security_group" "public" {
+  name = "public"
+  description = "Just SSH and HTTP"
+
+  # SSH access from anywhere
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  instance_type = "t2.micro"
+  name = "Grafana"
+
+  ami = "${var.grafana-ami}"
+  security_groups = ["${aws_security_group.public.name}"]
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,6 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "grafana-ami" {
+  default = "ami-adbba7e8"
+}
