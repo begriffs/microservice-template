07b94c0e7f72a04f356b349f328e94eb84a4e1d8
Joe Nelson <cred+github@begriffs.com>
2015-01-12 01:36:19 -0800

    Influxdb box

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -9,6 +9,10 @@ variable "consul-ami" {
   default = "ami-e3c2dda6"
 }
 
+variable "influx-ami" {
+  default = "ami-6f98872a"
+}
+
 variable "key_name" {
   description = "Name of the keypair to use in EC2."
   default = "terraform"
